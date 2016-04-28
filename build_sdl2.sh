f177f57318532080bbf5b184d67bb7fc881f8253
Kasper Lind Sørensen <kasper.lind.sorensen@gmail.com>
2016-04-28 23:01:14 +0200

    Added sdl2_ttf and sdl2_image to build_sdl2.sh

--- build_sdl2.sh
+++ build_sdl2.sh
@@ -7,3 +7,20 @@ cd build
 make
 sudo make install
 cd ../..
+
+hg clone https://hg.libsdl.org/SDL_ttf/ SDL_ttf
+cd SDL_ttf
+mkdir build
+cd build
+../configure
+make
+sudo make install
+cd ../..
+
+hg clone https://hg.libsdl.org/SDL_image/ SDL_image
+cd SDL_image
+mkdir build
+cd build
+../configure
+make
+sudo make install
