d6f472dd9ae6cc21501c65f1240d1e138d8850d3
Kasper Lind Sørensen <kasper.lind.sorensen@gmail.com>
2016-04-30 22:14:39 +0200

    build_sdl2.sh now gets SDL2_ttf and SDL2_image from tar files rather than mercurial.

--- build_sdl2.sh
+++ build_sdl2.sh
@@ -8,8 +8,9 @@ make
 sudo make install
 cd ../..
 
-hg clone https://hg.libsdl.org/SDL_ttf/ SDL_ttf
-cd SDL_ttf
+wget https://www.libsdl.org/projects/SDL_ttf/release/SDL2_ttf-2.0.14.tar.gz
+tar -xvf SDL2_ttf-2.0.14.tar.gz
+cd SDL2_ttf-2.0.14
 mkdir build
 cd build
 ../configure
@@ -17,8 +18,9 @@ make
 sudo make install
 cd ../..
 
-hg clone https://hg.libsdl.org/SDL_image/ SDL_image
-cd SDL_image
+wget https://www.libsdl.org/projects/SDL_image/release/SDL2_image-2.0.1.tar.gz
+tar -xvf SDL2_image-2.0.1.tar.gz
+cd SDL2_image-2.0.1
 mkdir build
 cd build
 ../configure
